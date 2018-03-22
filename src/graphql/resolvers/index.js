9493d54d52b0d7bf542dc2695638ae72253acae8
Josep Otal <j.otal.pares@gmail.com>
2018-03-21 23:14:15 -0600

    Add user model and schema and add authentication

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,15 +1,19 @@
 import GraphQLDate from 'graphql-date';
 import TweetResolvers from './tweet-resolvers';
+import UserResolvers from './user-resolvers';
 
 export default {
   Date: GraphQLDate,
   Query: {
     getTweet: TweetResolvers.getTweet,
     getTweets: TweetResolvers.getTweets,
+    me: UserResolvers.me,
   },
   Mutation: {
     createTweet: TweetResolvers.createTweet,
     updateTweet: TweetResolvers.updateTweet,
     deleteTweet: TweetResolvers.deleteTweet,
+    signup: UserResolvers.signup,
+    login: UserResolvers.login,
   },
 };
